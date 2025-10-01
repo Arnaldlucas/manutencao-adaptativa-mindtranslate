8a10308427014f894ad6c1cf9a0b94077263aa00
Arnald Lucas <arnaldff@gmail.com>
2025-10-01 08:59:43 -0400

    feat: complete PWA implementation and add evidence

--- CodigoFonte/vite.config.js
+++ CodigoFonte/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa"; // Importe o plugin
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      // Adicione a configuração do PWA aqui
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "MindTranslate",
+        short_name: "MindTranslate",
+        description: "Plataforma para aprender termos técnicos de programação.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
